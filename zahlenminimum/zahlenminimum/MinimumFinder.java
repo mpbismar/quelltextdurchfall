f29f1e54375e0882e62c4feddbc69ca6bd74bb82
Karl Wolf <wolfkarl@gmail.com>
2013-05-16 11:29:20 +0200

    Zahlenminimum fertig!

--- zahlenminimum/zahlenminimum/MinimumFinder.java
+++ zahlenminimum/zahlenminimum/MinimumFinder.java
@@ -6,8 +6,36 @@ public class MinimumFinder {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//In myIn = new In();
+		System.out.println("Usage: beliebig viele ganze Zahlen eingeben, Ende mit negativer Zahl!");
+		In.init();
+		
+		int minimum = 0;
+		int aktuell = 0;
+		int count = 0;
+		
+		do
+		{
+			aktuell = In.getInt();
+			
+			if(((aktuell < minimum) && (aktuell > 0)) || count == 0)
+			{
+				minimum = aktuell;
+			}
+			count++;
+		}
+		while(aktuell >= 0);
+		
+		if(count == 1)
+		{
+			System.out.println("Keine Eingabe - kein Minimum!");
+		}
+		else
+		{
+			System.out.print("Minimum: ");
+			System.out.println(minimum);
+		}
 	}
 
 }
