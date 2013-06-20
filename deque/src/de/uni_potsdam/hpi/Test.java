14b519d3bdd49a20223282312d49fb74ff344f2c
Karl Wolf <wolfkarl@gmail.com>
2013-06-20 23:49:51 +0200

    Laufzeitanalyse und Text

--- deque/src/de/uni_potsdam/hpi/Test.java
+++ deque/src/de/uni_potsdam/hpi/Test.java
@@ -6,28 +6,108 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Deque testList = new DequeRing(1000);
-		// Deque testRing = new DequeRing(1000);
-		
-		for (int i=1; i<=10; i++)
-		{
-			int e = i;
-			try {
-				testList.addFirst(e);
-				System.out.println("yo." + i);
-			} catch (DequeFull e1) {
-				e1.printStackTrace();
-			}
-		}
-		
-		for (int i=1; i<=10; i++)
-		{
-			try {
-				System.out.println(testList.removeFirst());
-			} catch (DequeEmpty e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
+		laufzeitanalyse();
+
+		}
+
+		public static void laufzeitanalyse ()
+		{
+		DequeRing testRing = new DequeRing(1000);
+		DequeList testList = new DequeList(1000);
+
+		double time1 = java.lang.System.nanoTime();
+
+		for (int i=1; i<=500; i++)
+		{
+		int e = i;
+		try {
+		testList.addFirst(e);
+		//System.out.println("added: " + e);
+		} catch (DequeFull e1) {
+		e1.printStackTrace();
+		}
+		}
+		double time2 = java.lang.System.nanoTime();
+		System.out.println("runtime Liste mit 500 Elementen befuellt: "+ ((time2-time1)/1000000) + " ms");
+
+		double time3 = java.lang.System.nanoTime();
+
+		for (int i=1; i<=500; i++)
+		{
+		int e = i;
+		try {
+		testRing.addFirst(e);
+		//System.out.println("added: " + e);
+		} catch (DequeFull e1) {
+		e1.printStackTrace();
+		}
+		}
+		double time4 = java.lang.System.nanoTime();
+		System.out.println("runtime Ring mit 500 Elementen befuellt: "+ ((time4-time3)/1000000) + " ms");
+		System.out.println("");
+		
+		
+		
+		String leer = " ";
+		
+		double time5 = java.lang.System.nanoTime();
+		for (int i=1; i<=500; i++)
+		{
+			if (i%2 == 1)
+			{	
+				 try {
+				 testList.removeLast();
+//				 System.out.println("removed:");
+				 } catch (DequeEmpty e) {
+				 e.printStackTrace();
+				 }
+			} 
+			else
+			{
+				
+				try {
+					testList.addLast(leer);
+//					System.out.println("added: ");
+					} catch (DequeFull e1) {
+					e1.printStackTrace();
+					}
+			}	
+			
+		}
+		double time6 = java.lang.System.nanoTime();
+		System.out.println("runtime je 250mal Element aus Liste entfernt und Leerstring hinzugefuegt : "+ ((time6-time5)/1000000) + " ms");
+
+		double time7 = java.lang.System.nanoTime();
+		for (int i=1; i<=500; i++)
+		{
+			if (i%2 == 1)
+			{	
+				 try {
+				 testRing.removeLast();
+//				 System.out.println("removed:");
+				 } catch (DequeEmpty e) {
+				 e.printStackTrace();
+				 }
+			} 
+			else
+			{
+				
+				try {
+					testRing.addLast(leer);
+//					System.out.println("added: ");
+					} catch (DequeFull e1) {
+					e1.printStackTrace();
+					}
+			}	
+			
+		}
+		double time8 = java.lang.System.nanoTime();
+		System.out.println("runtime je 250mal Element aus Ring entfernt und Leerstring hinzugefuegt : "+ ((time8-time7)/1000000) + " ms");
+		
+		System.out.println("test: Anzahl Ringelemente, Listenelemente: " + testRing.currentSize + "," + testList.currentSize);
+		
+		
+	}
+			
 }
