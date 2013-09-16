daf4c0427e689722564c590e95577f2561a3838a
Karl Wolf <wolfkarl@gmail.com>
2013-09-16 14:24:26 +0200

    Mensch Klasse ausgelagert & Konstruktor gebaut

--- EinsteinsJava/src/Einsteinraetsel.java
+++ EinsteinsJava/src/Einsteinraetsel.java
@@ -1,49 +1,15 @@
-
 public class Einsteinraetsel {
 
 	/**
 	 * @param args
 	 */
 
-	public static enum HOBBY {KLAVIERSPIELEN, SCHACHSPIELEN, FUSSBALLSPIELEN, HANDBALLSPIELEN};
-	public static enum ALTER {ELF,ZWOELF,DREIZEHN,VIERZEHN};
-	public static enum WOHNORT {MAINZ, LEIPZIG, BERLIN, HAMBURG};
-	public static enum NAME {DAVID, ALEXANDER, LUKAS, FLORIAN};
-	
-	
-	
-	public class Mensch
-	{
-		NAME name;
-		HOBBY hobby;
-		ALTER alter;
-		WOHNORT wohnort;
-		
-		public void setName(NAME newname) 	{name = newname;}	
-		public void setHobby(HOBBY newhobby) 	{hobby = newhobby;}	
-		public void setAlter(ALTER newalter) 	{alter = newalter;}	
-		public void setWohnort(WOHNORT newwohnort) 	{wohnort = newwohnort;}	
-		
-		public NAME getName() 	{return name;}	
-		public HOBBY getHobby() 	{return hobby;}	
-		public ALTER getAlter() 	{return alter;}	
-		public WOHNORT getWohnort() 	{return wohnort;}
-		
-		
-		public void print()
-		{
-			System.out.println(name +" :   Hobby: "+hobby+", Alter: "+alter+", Wohnort: "+wohnort);
-		}
-		
-	}
-	
-	
 	
 	public static void main(String[] args) {
 
 
 		Mensch David = new Mensch();
-		David.setName(NAME.DAVID);
+		David.setName(Mensch.NAME.DAVID);
 		
 		Mensch Alexander = new Mensch();
 		Alexander.setName(NAME.ALEXANDER);
