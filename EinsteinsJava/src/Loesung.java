ffe23dd2195ca5cb1ab2aff7a7ded1d173918136
Karl Wolf <wolfkarl@gmail.com>
2013-09-16 14:30:20 +0200

    Loesungsklasse & Konstruktor erstellt

--- EinsteinsJava/src/Loesung.java
+++ EinsteinsJava/src/Loesung.java
@@ -1,4 +1,23 @@
 
+/**
+ * 
+ * @author Karl & Paul
+ * 
+ * Enthaelt eine moegliche Loesung fuer das Raetsel, bestehend aus
+ * vier Mensch-Objekten
+ *
+ */
 public class Loesung {
 
+	Mensch[] menschen;
+	
+	Loesung(Mensch m1, Mensch m2, Mensch m3, Mensch m4)
+	{
+		menschen = new Mensch[4];
+		menschen[0] = m1;
+		menschen[1] = m2;
+		menschen[2] = m3;
+		menschen[3] = m4;
+	}
+	
 }
